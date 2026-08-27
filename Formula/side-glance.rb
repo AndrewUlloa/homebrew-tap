f88ecefd7184b04915d8b7dec8f1e8cfe0179471
Andrew Ulloa <aulloa2017@fau.edu>
2026-08-27 15:32:29 -0400

    Update Side Glance to 0.1.0-beta.11 (#9)

--- Formula/side-glance.rb
+++ Formula/side-glance.rb
@@ -9,26 +9,26 @@ class SideGlance < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/AndrewUlloa/side-glance/releases/download/v0.1.0-beta.10/side-glance-v0.1.0-beta.10-darwin-arm64.tar.gz"
-      sha256 "4208739b011e0bbe3a1d68a481a1f4a7213b644bc1496ca9b566f199b14a746c"
+      url "https://github.com/AndrewUlloa/side-glance/releases/download/v0.1.0-beta.11/side-glance-v0.1.0-beta.11-darwin-arm64.tar.gz"
+      sha256 "9a2697a190aa093232d7c4d016ba5cd91d55de5bbb118fa786892f4db0874f40"
     end
 
     on_intel do
       # Node SEA does not regularly test Intel macOS; this beta artifact is experimental.
-      url "https://github.com/AndrewUlloa/side-glance/releases/download/v0.1.0-beta.10/side-glance-v0.1.0-beta.10-darwin-x64.experimental.tar.gz"
-      sha256 "f0f57f909051d8752d4383fcda0808187c947bffae8698c855873f7adac85e80"
+      url "https://github.com/AndrewUlloa/side-glance/releases/download/v0.1.0-beta.11/side-glance-v0.1.0-beta.11-darwin-x64.experimental.tar.gz"
+      sha256 "964675b12212524d2fd13e517488d7a6a93bc30900f24274373621d6eb135d54"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/AndrewUlloa/side-glance/releases/download/v0.1.0-beta.10/side-glance-v0.1.0-beta.10-linux-arm64-gnu.tar.gz"
-      sha256 "0e01ea2b74b7ca59da7f8a5eb5e7d4b66da68bf5879181e59ec9755959b993f5"
+      url "https://github.com/AndrewUlloa/side-glance/releases/download/v0.1.0-beta.11/side-glance-v0.1.0-beta.11-linux-arm64-gnu.tar.gz"
+      sha256 "449f4e694e0f913e7ca625204ddcf5126690f4ca46ea3db5b0157a3ba4709b18"
     end
 
     on_intel do
-      url "https://github.com/AndrewUlloa/side-glance/releases/download/v0.1.0-beta.10/side-glance-v0.1.0-beta.10-linux-x64-gnu.tar.gz"
-      sha256 "fd3dcc885c143df5b5f3e12d13071efda08fe24425c70eed13818a360f539fd6"
+      url "https://github.com/AndrewUlloa/side-glance/releases/download/v0.1.0-beta.11/side-glance-v0.1.0-beta.11-linux-x64-gnu.tar.gz"
+      sha256 "2419a63f46c7e67d3d4ced765dce94b5fbd1fdc9a3b3cb2a4878d71765947160"
     end
   end
 
